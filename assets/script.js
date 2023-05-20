53acc06e5c446e115b189605ff58274264fc5e83
samt11345 <Samuel.tahjai.williams@gmail.com>
2023-05-20 09:12:33 -0400

    i added some javascript to thr prework study guide

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var topics = ['HTML', 'css', 'git', 'JavaSript']; 
+for (var x = 0; x < topics.length; x++) {
+  console.log(topics[x]);
+}
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'css') {
+  console.log("let's study css!");
+} else if (topic === 'git') {
+  console.log("lets study git!");
+} else if (topic === 'javascript') {
+  console.log("let's study javascript!");
+} else {
+  console.log('plese try again!')
+}
+*/
+
+
+
